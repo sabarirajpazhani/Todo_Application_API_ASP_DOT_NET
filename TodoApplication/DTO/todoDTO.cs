cdf054b616d71fcb02920619f8fb1d6578e25835
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-09-08 19:15:29 +0530

    Fix: DTO file

---
+++ TodoApplication/DTO/todoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApplication.DTO
+{
+    public class todoDTO
+    {
+        public string title { get; set; }   
+        public string description { get; set; }
+        public string isComplete { get; set; }  
+    }
+}
